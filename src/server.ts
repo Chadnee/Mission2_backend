8ff37ab6d818bae39172c55f0c8dfb623f7d59a6
Mstchadnee <116938357+Mstchadnee@users.noreply.github.com>
2026-02-10 13:01:05 +0600

    fixed error

--- src/server.ts
+++ src/server.ts
@@ -17,10 +17,10 @@ async function main() {
       autoIndex: true
     });
    await seedSuperAdmin() //check is super admin exist? if not, it will create super admin to the first user 
-   server = app.listen(config.port, () => {
-      console.log( `app listening on port ${config.port}`);
+  //  server = app.listen(config.port, () => {
+  //     console.log( `app listening on port ${config.port}`);
 
-    });
+  //   });
   } catch (err) {
     console.log(err);
   }
@@ -28,16 +28,16 @@ async function main() {
 
 main();
 
-process.on('unhandledRejection', ()=> {
-  console.log(`unhandledRejection is detected, shutting down... `);
+// process.on('unhandledRejection', ()=> {
+//   console.log(`unhandledRejection is detected, shutting down... `);
 
- if (server) {
-  server.close(() => {
-    process.exit(1);
-  })
- } 
-  process.exit(1);
-})
+//  if (server) {
+//   server.close(() => {
+//     process.exit(1);
+//   })
+//  } 
+//   process.exit(1);
+// })
 
 
 process.on('uncaughtException', () => {
