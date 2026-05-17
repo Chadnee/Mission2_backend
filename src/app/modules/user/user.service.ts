0966578ac1fbb9367796c6f60307c3d1df470402
Mstchadnee <116938357+Mstchadnee@users.noreply.github.com>
2026-05-17 09:50:12 +0600

    test

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -217,7 +217,7 @@ const getMeFromDB = async(userId: string, role: string)=>{
      if(role === 'faculty'){
         result = await Faculty.findOne({id: userId})
      };
-     console.log(result)
+     console.log(result, role)
      return result;
 }
 
